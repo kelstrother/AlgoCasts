efb4b9c6340036f4001365695d0d78dfc19cb0dd
Kel Strother <kelstrother@gmail.com>
2021-07-31 13:41:32 -0600

    reverse string solved

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -1,11 +1,24 @@
 // --- Directions
-// Given a string, return a new string with the reversed
-// order of characters
+//! Given a string, return a new string with the reversed
+//! order of characters
 // --- Examples
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//~ \\\\\\\\\\.REVERSE METHOD\\\\\\\\\\\
+//! function reverse(str) {
+// !   const arr = str.split('');
+//!    arr.reverse();
+//!   return arr.join('');
+//! }
+//~ \\\\\\\\\ CLEANED UP \\\\\\\\\\\\
+//! function reverse(str) {
+//!   return str
+//!       .split('')
+//!      .reverse()
+//!      .join('');
+//! }
+
 
 module.exports = reverse;
